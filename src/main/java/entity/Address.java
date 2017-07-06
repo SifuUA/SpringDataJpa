cae0b7c68fdb68bc89a375aa2d9a777f6c324326
SifuUA <alekseykres@gmail.com>
2017-07-06 23:01:29 +0300

    add entity

---
+++ src/main/java/entity/Address.java
@@ -0,0 +1,91 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by Alex on 06.07.2017.
+ */
+
+@Entity
+@Table (name = "ADDRESS")
+public class Address implements Serializable{
+
+    private static final long serialVersionUID = -5749235697982514914L;
+
+    @Id
+    private Long id;
+
+    @Column (name = "COUNTRY")
+    private String country;
+
+    @Column (name = "CITY")
+    private String city;
+
+    @Column (name = "STREET")
+    private String street;
+
+    @Column (name = "POST_CODE", length = 10)
+    private String postCode;
+
+    public Address() {
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", postCode='" + postCode + '\'' +
+                '}';
+    }
+}
